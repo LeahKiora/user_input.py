ab09f99fb4623e14a84d74fcdf0d53364cc9458f
leah kiora <leahkiora@gmail.com>
2024-03-18 22:38:33 +0300

    first commit

---
+++ age.py
@@ -0,0 +1,7 @@
+#Prompt user to enter their name
+age = int(input("Enter your age:"))
+
+#Check if the age is greater or equal to
+if age >= 18:
+    print("You are eligible to vote")
+else:print("You are not eligible to vote")
